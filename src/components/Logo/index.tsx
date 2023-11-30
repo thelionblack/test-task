9a0910635d952a282ecca5fb7e294088f79b8e72
Maksim <thelionblack.com@gmail.com>
2023-11-30 15:35:21 +0300

    refactor: add constants

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,10 +6,13 @@ enum classNames {
   logoSubtext = `${classNames.logo}__subtext`
 }
 
+const LOGO_TEXT = 'CAT';
+const LOGO_SUB_TEXT = 'currencies academic terms';
+
 const Logo = () => (
   <div className={classNames.logo}>
-    <h1 className={classNames.logoText}>CAT</h1>
-    <p className={classNames.logoSubtext}>currencies academic terms</p>
+    <h1 className={classNames.logoText}>{LOGO_TEXT}</h1>
+    <p className={classNames.logoSubtext}>{LOGO_SUB_TEXT}</p>
   </div>
 );
 
