cb92b470f4d8be2a291f6731b082074c0caac334
Maksim <thelionblack.com@gmail.com>
2023-11-30 10:27:21 +0300

    feat: add base layout

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@/assets/styles/index.css'
+
+const App = () => {
+    return (
+        <div>
+            123
+        </div>
+    );
+};
+
+export default App;
