0a68a2061f6546b4c6ab9f114e89563e5b9e0426
Maksim <thelionblack.com@gmail.com>
2023-11-30 10:59:22 +0300

    feat: add Logo component and some styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '@/assets/styles/index.css'
+import Logo from "@/components/Logo";
 
 const App = () => {
     return (
         <div className='block'>
             <div className='block__element--top'>
-
+                <div>
+                    <Logo />
+                </div>
             </div>
             <div className='block__element--bottom'>
                 <p className='currency__title'>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <h1 className='logo__text'>CAT</h1>
+            <p className='logo__subtext'>currencies academic terms</p>
+        </div>
+    );
+};
+
+export default Logo;
